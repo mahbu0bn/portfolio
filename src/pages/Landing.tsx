576611cf84df7634bb0eaf47c354acc5a9ed157d
Kamyar Azizi <kamyar2019azizi@gmail.com>
2026-03-26 14:30:14 +0100

    feat: new homepage image

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -11,26 +11,26 @@ const Landing: React.FC = () => {
       transition={{duration: 1.2, ease: "easeOut"}}
     >
       {/* Background Image */}
-      {/* <div
-        className="absolute inset-0 bg-cover bg-center brightness-[0.95] contrast-[1.05]"
+      <div
+        className="absolute rotate-90 inset-0 bg-cover bg-center brightness-[0.95] contrast-[1.05]"
         style={{
-          backgroundImage: `url('/images/animated.gif')`,
+          backgroundImage: `url('/images/homepage.jpg')`,
         }}
-      /> */}
-      <video
+      />
+      {/* <video
         className="absolute inset-0 w-full h-full object-cover brightness-[0.95] contrast-[1.05]"
         autoPlay
         muted
         playsInline
       >
         <source src="/images/animated.mp4" type="video/mp4" />
-      </video>
+      </video> */}
 
       {/* Overlay - Increased opacity for better contrast */}
-      <div className="absolute inset-0 bg-white/15" />
+      {/* <div className="absolute inset-0 bg-white/15" /> */}
 
       {/* Content Container - Adjusted for mobile */}
-      <div className="absolute inset-0 flex flex-col items-center justify-center p-4 text-neutral-900">
+      <div className="absolute inset-0 flex flex-col items-center justify-center p-4 text-white">
         <motion.h1
           className="mb-4 text-4xl font-light tracking-wider text-center md:text-8xl"
           initial={{opacity: 0, y: 20}}
@@ -42,7 +42,7 @@ const Landing: React.FC = () => {
         </motion.h1>
 
         <motion.p
-          className="text-base md:text-xl tracking-[0.3em] mb-12 md:mb-16 font-normal text-neutral-800 text-center"
+          className="text-base md:text-xl tracking-[0.3em] mb-12 md:mb-16 font-normal text-white text-center"
           initial={{opacity: 0, y: 15}}
           animate={{opacity: 1, y: 0}}
           transition={{duration: 0.8, delay: 0.5, ease: "easeOut"}}
