55ce2a47fcb8ac7111ec75fd80b43c5ea3b482b0
Kamyar Azizi <kamyar2019azizi@gmail.com>
2026-04-07 20:28:29 +0200

    feat: Update landing page design and background image

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,69 +1,65 @@
 import React from "react";
-import {Link} from "react-router-dom";
 import {motion} from "framer-motion";
+import {useNavigate} from "react-router-dom";
 
 const Landing: React.FC = () => {
+  const navigate = useNavigate();
+  const navigateOnClick = () => {
+    navigate("/selected");
+  };
   return (
     <motion.div
-      className="relative w-screen h-screen overflow-hidden"
+      className="relative flex flex-col items-center justify-center w-screen h-screen mx-20 overflow-hidden"
       initial={{opacity: 0}}
       animate={{opacity: 1}}
       transition={{duration: 1.2, ease: "easeOut"}}
     >
-      {/* Background Image */}
-      <div
-        className="absolute rotate-90 inset-0 bg-cover bg-center brightness-[0.95] contrast-[1.05]"
-        style={{
-          backgroundImage: `url('/images/homepage.jpg')`,
-        }}
-      />
-      {/* <video
-        className="absolute inset-0 w-full h-full object-cover brightness-[0.95] contrast-[1.05]"
-        autoPlay
-        muted
-        playsInline
-      >
-        <source src="/images/animated.mp4" type="video/mp4" />
-      </video> */}
-
-      {/* Overlay - Increased opacity for better contrast */}
-      {/* <div className="absolute inset-0 bg-white/15" /> */}
-
-      {/* Content Container - Adjusted for mobile */}
-      <div className="absolute inset-0 flex flex-col items-center justify-center p-4 text-white">
-        <motion.h1
-          className="mb-4 text-4xl font-light tracking-wider text-center md:text-8xl"
-          initial={{opacity: 0, y: 20}}
-          animate={{opacity: 1, y: 0}}
-          transition={{duration: 0.8, delay: 0.3, ease: "easeOut"}}
-          aria-label="Mahboube"
+      <div className="relative flex items-center justify-center w-full overflow-hidden h-max">
+        {/* Text */}
+        <div
+          className="z-50 flex flex-col items-end justify-center w-3/5 min-h-[60vh] overflow-hidden bg-cover bg-right bg-no-repeat"
+          style={{
+            backgroundImage: "url('/images/homepage.png')",
+          }}
         >
-          MAHBOUBE
-        </motion.h1>
-
-        <motion.p
-          className="text-base md:text-xl tracking-[0.3em] mb-12 md:mb-16 font-normal text-white text-center"
-          initial={{opacity: 0, y: 15}}
-          animate={{opacity: 1, y: 0}}
-          transition={{duration: 0.8, delay: 0.5, ease: "easeOut"}}
-        >
-          CONTEMPORARY CALLIGRAPHY
-        </motion.p>
-
-        <motion.div
-          initial={{opacity: 0, y: 15}}
-          animate={{opacity: 1, y: 0}}
-          transition={{duration: 0.8, delay: 0.7, ease: "easeOut"}}
-        >
-          <Link
-            to="/selected"
-            className="px-8 py-3 text-sm font-medium tracking-widest transition-all duration-300 border-2 border-neutral-600 md:px-16 hover:bg-neutral-900 hover:text-white hover:border-neutral-900 text-neutral-800"
-            aria-label="Enter site"
+          {/* Background Image */}
+          {/* <img
+            src="/images/homepage.png"
+            alt="Background"
+            className="fixed w-[75%] rotate-90 -left-[100px] top-1/2 -translate-y-1/2"
+          /> */}
+          <motion.h1
+            initial={{opacity: 0, y: 15}}
+            animate={{opacity: 1, y: 0}}
+            transition={{duration: 0.8, delay: 0.5, ease: "easeOut"}}
+            onClick={navigateOnClick}
+            className="z-20 flex flex-col gap-2 text-4xl font-bold text-white cursor-pointer text-start md:text-6xl"
           >
-            ENTER
-          </Link>
-        </motion.div>
+            <span>MAHB</span>
+            <span>ART</span>
+          </motion.h1>
+        </div>
+        <div className="w-1/5">
+          <motion.h1
+            initial={{opacity: 0, y: 15}}
+            animate={{opacity: 1, y: 0}}
+            transition={{duration: 0.8, delay: 0.5, ease: "easeOut"}}
+            onClick={navigateOnClick}
+            className="z-20 flex flex-col gap-2 text-4xl font-bold text-black cursor-pointer text-start md:text-6xl"
+          >
+            <span className="text-black">OUBE</span>
+            <span className="text-black">STUDIO</span>
+          </motion.h1>
+        </div>
       </div>
+      <motion.p
+        className="text-base md:text-xl tracking-[0.3em] mt-2 md:mt-4 font-normal text-black w-4/5 text-end"
+        initial={{opacity: 0, y: 15}}
+        animate={{opacity: 1, y: 0}}
+        transition={{duration: 0.8, delay: 0.5, ease: "easeOut"}}
+      >
+        CONTEMPORARY CALLIGRAPHY
+      </motion.p>
     </motion.div>
   );
 };
