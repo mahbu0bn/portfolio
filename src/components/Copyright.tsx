2f8eb8b918a4cccf7ef7f4ae990c58a270d7a665
Kamyar Azizi <kamyar2019azizi@gmail.com>
2026-04-09 17:13:34 +0200

    fix: text position in small screens

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,6 +1,6 @@
 export default function Copyright() {
   return (
-    <p className="w-full px-4 text-sm text-end md:text-base">
+    <p className="w-full px-4 text-sm text-center md:text-end md:text-base">
       © 2026 Mahboube Najafi. All rights reserved.
     </p>
   );
